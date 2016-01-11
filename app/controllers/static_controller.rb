4dee7da32e7a7ca039bfca553b05217877f6bed7
Irene Lee <sohyeonlee@berkeley.edu>
2016-01-10 18:58:40 -0800

    result page

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -7,14 +7,14 @@ class StaticController < ApplicationController
 
   def search
     @ride = Ride.new
-    render "result"
+    redirect_to results_path
   end
 
   def result
     @rides = Ride.where(nil)
-    filtering_params(params).each do |key, value|
-      @rides = @rides.public_send(key, value) if value.present?
-    end
+    # filtering_params(params).each do |key, value|
+    #   @rides = @rides.public_send(key, value) if value.present?
+    # end
   end
 
 private
